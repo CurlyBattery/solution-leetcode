d04ad7a4697ed21f110f4e78594b180775913ea7
Kosyrev143 <artemkosyrev93@gmail.com>
2025-08-02 09:33:53 +0500

    feat: (remove)

--- src/removeElement.ts
+++ src/removeElement.ts
@@ -1,2 +1,12 @@
+let arr = [0,1,2,2,3,0,4,2] ;
+const value = 2;
 
+function removeElement(nums: number[], val: number): number | void {
+    while(nums.includes(val)) {
+        const indexValue = nums.findIndex((x) => x === val);
+        nums.splice(indexValue, 1);
+    }
+    return nums.length;
 };
+
+console.log(removeElement(arr, value))
