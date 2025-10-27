6698352016eb93cb298ed778959186d1177daaaa
Kosyrev143 <artemkosyrev93@gmail.com>
2025-10-27 21:48:17 +0500

    feat: (validParenthes)

---
+++ src/validParenthes.ts
@@ -0,0 +1,41 @@
+function validParenthes(s: string): boolean | void {
+    let isValid = true;
+    for (let i = 0; i < s.length; i++) {
+        let idxSecond: number;
+        switch (s[i]) {
+            case '(':
+                idxSecond = s.indexOf(')');
+                s = s.substring(0, idxSecond) + s.substring(idxSecond + 1);
+                if(idxSecond === -1) {
+                    isValid = false;
+                }
+                break;
+            case '{':
+                idxSecond = s.indexOf('}');
+                console.log(idxSecond)
+                s = s.substring(0, idxSecond) + s.substring(idxSecond + 1);
+                console.log(s)
+                if(idxSecond === -1) {
+                    isValid = false;
+                }
+                break;
+            case '[':
+                idxSecond = s.indexOf(']');
+                s = s.substring(0, idxSecond) + s.substring(idxSecond + 1);
+                if(idxSecond === -1) {
+                    isValid = false;
+                }
+                break;
+            case ']':
+            case '}':
+            case ')':
+                isValid = false;
+                break;
+        }
+    }
+
+    return isValid;
+}
+
+
+console.log(validParenthes("(])]"));
